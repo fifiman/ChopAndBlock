4045abaa79ec6e1873a7d5e631efe7823244f921
Nenad Bauk <bauk.nenad@gmail.com>
2017-11-15 01:41:34 +0100

    Add more comments/docstrings. Fixes #9.

--- channel_protocol.py
+++ channel_protocol.py
@@ -1,6 +1,6 @@
 import asynchat
-import cPickle as pickle
 import logging
+import cPickle as pickle
 
 
 class ChannelProtocol(asynchat.async_chat):
@@ -8,12 +8,13 @@ class ChannelProtocol(asynchat.async_chat):
     ChannelProtocol implements underlying structure for client and server channels. In particular, handling of
     buffer terminator cases.
 
-    Args:
-        connection (socket): Connect to already existing socket.
-        socket_map (map[socket]): Socket map to which to be added to, if exists.
+    Commands can contain pickled binary data after them. If data follows a command, a colon will separate the
+    command and the length of the following pickled data. In this case, the channel terminator will be changed
+    from a newline to until this number of bytes have been read. After the data is read, the terminator will
+    return to it's default state.
 
     Attributes:
-        buffer ([str]): Buffer containing blocks of incoming data.
+        buffer ([str]): Buffer containing blocks of received data.
         mid_command (str/None): Previous command while we are waiting on binary data associated to that command.
     """
 
@@ -21,7 +22,13 @@ class ChannelProtocol(asynchat.async_chat):
     DEFAULT_TERMINATOR = "\n"
 
     def __init__(self, connection=None, socket_map=None):
+        """
+        Initialize channel protocol and it's parent class.
 
+        Args:
+            connection (socket): Connect to already existing socket.
+            socket_map (map[socket]): Socket map to which to be added to, if exists.
+        """
         asynchat.async_chat.__init__(self, sock=connection, map=socket_map)
 
         self.set_terminator(ChannelProtocol.DEFAULT_TERMINATOR)
@@ -30,18 +37,36 @@ class ChannelProtocol(asynchat.async_chat):
         self.mid_command = None
 
     def collect_incoming_data(self, data):
+        """
+        Append data to channel protocol buffer once it has been received.
+
+        Args:
+            data (str): Incoming data.
+
+        Returns:
+            None
+        """
         self.buffer.append(data)
 
     def found_terminator(self):
+        """
+        Process data in buffer after terminator has been found.
+
+        There are two possible cases for received buffer data:
+            1. Either a new command has been received, which may be followed by it's binary data.
+            2. Binary data for the previous command.
+
+        Returns: None
+        """
         buffer_data = ''.join(self.buffer)
         self.buffer[:] = []
 
         if self.mid_command is not None:
             # Unpickle data and process command
-
             command = self.mid_command
             data = pickle.loads(buffer_data)
 
+            # Reset channel protocol state.
             self.mid_command = None
             self.set_terminator(ChannelProtocol.DEFAULT_TERMINATOR)
 
@@ -49,13 +74,24 @@ class ChannelProtocol(asynchat.async_chat):
         else:
             command, dataLength = buffer_data.split(ChannelProtocol.COMMAND_SEPARATOR)
 
-            if dataLength:
+            if dataLength: # Binary data follows current command.
                 self.mid_command = command
                 self.set_terminator(int(dataLength))
             else:
                 self.process_command(command)
 
     def send_command(self, command, data=None):
+        """
+        Send command, optionally with according data. Pickling the data in case it exists.
+
+        Args:
+            command (str): Command to send.
+            data (None/str): Data to follow command, if it exists.
+
+        Returns:
+            None
+        """
+        # Add command separator if does not exist due to how commands are parsed.
         if ChannelProtocol.COMMAND_SEPARATOR not in command:
             command += ChannelProtocol.COMMAND_SEPARATOR
 
@@ -70,6 +106,16 @@ class ChannelProtocol(asynchat.async_chat):
             self.push(command + ChannelProtocol.DEFAULT_TERMINATOR)
 
     def process_command(self, command, data=None):
+        """
+        Process given command with according optional data.
+
+        Args:
+            command (str): Command to process.
+            data (None/str): Data to process with command, if exists.
+
+        Returns:
+            None or NotImplementedError if command does not exist.
+        """
         commands = {"disconnect" : lambda x, y : self.handle_close()}
 
         if command in commands:
@@ -79,4 +125,10 @@ class ChannelProtocol(asynchat.async_chat):
             raise NotImplementedError("This command does not exist: %s." % command)
 
     def handle_close(self):
+        """
+        Close channel.
+
+        Returns:
+             None
+        """
         self.close()
--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import asyncore
-import marshal
 import logging
+import marshal
 import socket
 import types
 
@@ -8,7 +8,20 @@ from channel_protocol import ChannelProtocol
 
 
 class Client(ChannelProtocol):
+    """
+    Client connects to server. Which immediately receives MapReduce functions to be used for current job. After
+    functions have been loaded, client will process multiple independent map/reduce jobs; sending results back
+    to the server every time.
+
+    Attributes:
+        map_fn (func): Map function.
+        collect_fn (func): Collect function.
+        reduce_fn (func): Reduce function.
+    """
     def __init__(self):
+        """
+        Initialize client and it's parent class.
+        """
         ChannelProtocol.__init__(self)
 
         self.map_fn = None
@@ -16,6 +29,16 @@ class Client(ChannelProtocol):
         self.collect_fn = None
 
     def connect_to_server(self, serverAddress, serverPort):
+        """
+        Connect client to server at given address, and process commands while connection is active.
+
+        Args:
+            serverAddress (str): Server address.
+            serverPort (int): Server port number.
+
+        Returns:
+            None
+        """
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((serverAddress, serverPort))
 
@@ -24,6 +47,19 @@ class Client(ChannelProtocol):
         asyncore.loop()
 
     def process_command(self, command, data=None):
+        """
+        Process given command with according optional data.
+
+        Client overrides this class from it's parent with more possible commands. In case client does not find
+        command, command is sent to be handled by parent class ChannelProtocol.
+
+        Args:
+            command (str): Command to process.
+            data (None/str): Data to process with command, if exists.
+
+        Returns:
+            None or NotImplementedError if command does not exist in both client and channel protocol functions.
+        """
         logging.debug("Attempting to process command: %s." % command)
 
         commands = {
@@ -40,6 +76,16 @@ class Client(ChannelProtocol):
             ChannelProtocol.process_command(self, command, data)
 
     def map(self, command, data):
+        """
+        Map given data based on loaded map function and send results to server.
+
+        Args:
+            command (str): Command being processed, not relevant to current mapping process.
+            data (str): Data being mapped.
+
+        Returns:
+            None
+        """
         logging.debug("Mapping %s." % data[0])
         results = {}
 
@@ -52,30 +98,76 @@ class Client(ChannelProtocol):
         self.send_command("map_done", (data[0], results))
 
     def reduce(self, command, data):
+        """
+        Reduce mapped data based on loaded reduce function, and send results to server.
+
+        Args:
+            command (str): Command being processed, not relevant to current reducing process.
+            data (str): Data being reduced.
+
+        Returns:
+            None
+        """
         logging.debug("Reducing %s." % data[0])
         results = self.reduce_fn(data[0], data[1])
 
         self.send_command("reduce_done", (data[0], results))
 
     def set_map(self, command, data):
+        """
+        Set map function to be used by client.
+
+        Args:
+            command (str): Command currently being process.
+            data (str): Binary version of function code.
+
+        Returns:
+            None
+        """
         map_code = marshal.loads(data)
         self.map_fn = types.FunctionType(map_code, globals(), "map")
 
         logging.debug("Client map function set.")
 
     def set_reduce(self, command, data):
+        """
+        Set reduce function to be used by client.
+
+        Args:
+            command (str): Command currently being process.
+            data (str): Binary version of function code.
+
+        Returns:
+            None
+        """
         reduce_code = marshal.loads(data)
         self.reduce_fn = types.FunctionType(reduce_code, globals(), "reduce")
 
         logging.debug("Client reduce function set.")
 
     def set_collect(self, command, data):
+        """
+        Set collect function to be used by client.
+
+        Args:
+            command (str): Command currently being process.
+            data (str): Binary version of function code.
+
+        Returns:
+            None
+        """
         collect_code = marshal.loads(data)
         self.collect_fn = types.FunctionType(collect_code, globals(), "collect")
 
         logging.debug("Client collect function set.")
 
     def handle_close(self):
+        """
+        Close client.
+
+        Returns:
+             None
+        """
         logging.info("Client disconnecting.")
         self.close()
 
